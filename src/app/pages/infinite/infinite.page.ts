832cb1e950e6c1d8990ddbf766c4ff4bfcf87341
Jesus Enmanuel De La Cruz <edelacruz9713@gmail.com>
2020-09-14 15:59:17 -0400

    more components

--- src/app/pages/infinite/infinite.page.ts
+++ src/app/pages/infinite/infinite.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonInfiniteScroll} from '@ionic/angular';
 
 @Component({
   selector: 'app-infinite',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infinite.page.scss'],
 })
 export class InfinitePage implements OnInit {
+
+  @ViewChild(IonInfiniteScroll) inifiniteScroll: IonInfiniteScroll;
+
   data: any[] = Array(20);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  loadData(event) {
+
+    setTimeout(() => {
+
+      if(this.data.length > 50) {
+        this.inifiniteScroll.complete();
+        this.inifiniteScroll.disabled = true;
+        return;
+      }
+      const nuevoArr = Array(20);
+
+      this.data.push(...nuevoArr);
+      // event.target.complete();
+    }, 2000);
+
+    this.inifiniteScroll.complete();
+
+  }
 }
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -65,6 +65,11 @@ export class InicioPage implements OnInit {
       name: 'Infinite Scroll',
       redirectTo: '/infinite'
     },
+    {
+      icon: 'hammer-outline',
+      name: 'Input Forms',
+      redirectTo: '/input'
+    },
   ];
   constructor() { }
 
